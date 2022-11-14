0354f588fe72a77a6fb5366ff3d9359d02395af4
hari sainath <harisainath51@gmail.com>
2022-11-14 15:02:09 +0530

    login BE

--- screens/login.js
+++ screens/login.js
@@ -1,35 +1,100 @@
-import React from 'react';
-import { View,ScrollView,StyleSheet,Image } from 'react-native';
-import {
-  useFonts,
-  Lato_100Thin,
-  Lato_100Thin_Italic,
-  Lato_300Light,
-  Lato_300Light_Italic,
-  Lato_400Regular,
-  Lato_400Regular_Italic,
-  Lato_700Bold,
-  Lato_700Bold_Italic,
-  Lato_900Black,
-  Lato_900Black_Italic,
-} from '@expo-google-fonts/lato';
-
+import React,{useState,useCallback} from 'react';
+import { View,ScrollView,StyleSheet,Image,Text,TextInput,TouchableOpacity } from 'react-native';
+import axios from 'axios';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useFocusEffect } from '@react-navigation/native';
+import { BASE_URL } from './constants';
 import CustButton from './button';
-import CustInput from './textFields';
 
 function Login({navigation}) {
-  let [fontsLoaded] = useFonts({
-    Lato_100Thin,
-    Lato_100Thin_Italic,
-    Lato_300Light,
-    Lato_300Light_Italic,
-    Lato_400Regular,
-    Lato_400Regular_Italic,
-    Lato_700Bold,
-    Lato_700Bold_Italic,
-    Lato_900Black,
-    Lato_900Black_Italic,
-  });
+
+  const[isLoading,setIsLoading]=useState(false);
+  const [emailError,setemailError]=useState();
+  const [passwordError,setpasswordError]=useState();
+
+  const [email,setemail]=useState();
+  const [password,setpassword]=useState();
+
+
+  const getData = () =>{ try{
+    AsyncStorage.getItem('loginusername').then(value => {
+      if(value!=null)
+      {
+        navigation.navigate('Dashboard',{});
+      }
+    })
+  }catch(error){
+    console.log(error);
+  }
+}
+
+useFocusEffect(
+  useCallback(() => {
+    getData()
+  }, [])
+);
+
+  const removeItemValue= async (key)=> {
+    try {
+        await AsyncStorage.removeItem(key);
+        return true;
+    }
+    catch(exception) {
+        return false;
+    }
+}
+const LoginDetails = async () =>{
+  setIsLoading(true);
+  
+  if(email=='' || email=='undefined' || email==null)
+  {  setemailError("Please Enter Email");setIsLoading(false);return true;}
+  else
+    {setemailError("");}
+  
+  if(password=="" || password=='undefined' || password==null)
+  {setpasswordError("Please Enter password");setIsLoading(false);return true;}
+  else
+  {setpasswordError("");}
+
+
+  axios.post(BASE_URL+"login.php", {
+    LoginEmail: email,
+    LoginPassword: password,
+  }, {
+    headers: {
+      'LoginEmail': email,
+      'LoginPassword': password,
+    }
+  }).then(response => {
+    if(response.data.code==200){
+        setIsLoading(false);
+        try{
+            AsyncStorage.setItem("loginusername",response.data.loginusername);
+            AsyncStorage.setItem("loginmobile",response.data.loginmobile);
+            AsyncStorage.setItem("loginid",response.data.loginid);
+            AsyncStorage.setItem("loginemail",response.data.loginemail);
+            AsyncStorage.setItem("loginDesc",response.data.loginDesc);
+        }catch(error){
+            console.log(error);
+        }
+        removeItemValue("loginpassword");
+        navigation.navigate('Dashboard');
+      }
+      else
+      {
+        alert(response.data.message);
+        setIsLoading(false);
+        return true;
+      }
+    }).catch(error => {
+      //console.log('useeffect' + error);
+  }
+ );
+
+
+ 
+}
+
     return (
     <View style={{flex:1}}>
         
@@ -46,28 +111,51 @@ function Login({navigation}) {
                 padding:20
               }}
         >
-          <Image
-          style={styles.Logo}
-          source={require('../assets/images/logotd.png')}
-          resizeMode='contain'
-        />
-              <CustInput
-              width="95%"
-              placeholder="Email"
-              secure={false}
-              ></CustInput>
+          <Text style={{fontSize:20,textAlign:'center',justifyContent:'center',alignItems:'center',color:'#ffffff'}}><Text style={{fontSize:35,color:'#ffffff'}}>T</Text>ech <Text style={{fontSize:35,color:'#ffffff'}}>A</Text>yuda</Text>
 
-                <CustInput
-              width="95%"
-              placeholder="Password"
-              secure={true}
-              ></CustInput>
+              
+              <TextInput
+                    style={[styles.input,{width:"95%",}]}
+                    placeholder="Email"
+                    secureTextEntry={false}
+                    keyboardType="email-address"
+                    name="loginemail"
+                    placeholderTextColor={'#bdbbbb'}
+                    onChangeText={(text) => setemail(text)}
+                />
+
+                <View style={{alignItems:'flex-start',width:"92%"}}>
+                <Text style={{color:"red"}}>{emailError}</Text>
+                </View>
+
+                <TextInput
+                    style={[styles.input,{width:"95%",}]}
+                    placeholder="Password"
+                    placeholderTextColor={'#bdbbbb'}
+                    secureTextEntry={true}
+                    name="loginpassword"
+                    onChangeText={(text) => setpassword(text)}
+                />
+                <View style={{alignItems:'flex-start',width:"92%"}}>
+                <Text style={{color:"red"}}>{passwordError}</Text>
+                </View>
                    
                 <CustButton
-                onPressFunction={() => navigation.navigate('Dashboard')}
+                onPressFunction={LoginDetails}
                 title="Login"
                 width="95%"
+                loading={isLoading}
                 ></CustButton>
+
+                <View style={{flexDirection:"row",alignContent:"center",alignItems:"center",justifyContent:"center",width:"100%"}}>
+                <TouchableOpacity style={{marginTop:30}} onPress={()=>navigation.navigate('Register')}>
+                <Text style={{fontWeight:'bold',color:'#ffffff',fontSize:18,}}> Register ? </Text>
+                </TouchableOpacity> 
+                <TouchableOpacity style={{marginTop:30}}><Text> | </Text></TouchableOpacity> 
+                <TouchableOpacity style={{marginTop:30}} onPress={()=>navigation.navigate('ForgotPassword')}>
+                <Text style={{fontWeight:'bold',color:'#ffffff',fontSize:18,}}> Forgot Password ?</Text>
+                </TouchableOpacity>
+                </View>
         </View>
         </ScrollView>
 
@@ -84,7 +172,16 @@ const styles = StyleSheet.create({
     height:120,
     borderRadius:60,
   },
-  
+  input: {
+    margin: 8,
+    borderWidth: 1,
+    padding: 10,
+    borderColor:'#2e2d35',
+    color:"#ffffff",
+    fontWeight:'600',
+    borderRadius:10,
+    backgroundColor:'#413e4f'
+  },
 });
 
 export default Login;
